525fac5455621c470df53b9c646dab569b3c41f3
KONG41 <sokkongchan@gmail.com>
2020-10-24 21:28:47 +0700

    second update

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,27 +3,52 @@ import { Link } from 'react-router-dom';
 // import { Dropdown,Menu,Item, Toggle,DropdownButton,ItemText } from 'react-bootstrap';
 import {Parser} from 'html-react-parser';
 class Home extends Component {
+  
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+              {
+                  this.state.show? <div><h1>Hide and Show</h1></div> : null
+              }
+              <button onClick={()=>{this.setState({show:!this.state.show})}}>{ this.state.show? 'Hide' : 'Show'} Div</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
     userData;
     constructor(props){
         super(props);
         this.categoryFild = this.categoryFild.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.state = {
-            data:'',
+            data:"",
+            dataList: [],
+            show:true
         }
         };
      
     categoryFild(e){
         this.setState({
-            data: e.target.value
+            data: [e.target.value]
         })
     }
-    
+
     onSubmit(e){
         e.preventDefault()
+        let dataList=this.state.dataList
+        dataList.push(this.state.data)
+
         this.setState({
             data:'',
+            dataList
+        // ReactDOM.render(<option value="grapefruit">{this.state.data}</option>, document.getElementById('option'));
+        
         })
+        console.log(this.state)
     }
     
     componentDidMount(){
@@ -31,7 +56,7 @@ class Home extends Component {
 
         if(localStorage.getItem('user')){
             this.setState({
-                data: this.userData.data,
+                data: this.state.dataList,
             })
         }
         else{
@@ -44,6 +69,17 @@ class Home extends Component {
     componentWillUpdate(nextProps, nextState){
        localStorage.setItem('user',JSON.stringify(nextState));
     }
+   renderOption () {
+       
+     return  this.state.dataList.map((x) => (
+        <li>
+            <a>
+                {x}
+            </a>
+        </li>
+       ))
+       
+    }
 
     render() {
 
@@ -53,7 +89,7 @@ class Home extends Component {
 
         
 
-        // };
+        // };                                                                       
         
         // addCategory = () => {
           
@@ -61,15 +97,18 @@ class Home extends Component {
         return (
             <div className="home-con">
                 <div className="nav-bar">
-                            <select  className="select"  >
-                                 <option value="grapefruit">{this.state.data}</option>
-                            </select>
+                    <i class="fas fa-align-justify" onClick={()=>{this.setState({show:!this.state.show})}}>
+                           {
+                                    this.state.show?<ul> {this.renderOption()}  </ul>  : null
+                           }
+                            
+                    </i>
                     <Link to="/" className="logout">LogOut</Link>
                 </div>
                 <div className="add-box">
                     <label>Input Category</label>
                     <br/>
-                  
+                    
                     <input onChange={this.categoryFild} value={this.state.data} type="text"/>
                     <br/>
                     <button onClick={this.onSubmit}> ADD</button>
